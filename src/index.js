6e06933b8e7b336ef4ec618ed5731f2c962818c0
Shukri Adams <shukri.adams@gmail.com>
2020-04-02 17:24:01 +0200

    added version option

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const process = require('process');
+const process = require('process'),
+    minimist = require('minimist'),
+    jsonfile = require('jsonfile'),
+    path = require('path'),
+    argv = minimist(process.argv.slice(2));
 
+if (argv.version){
+    let package = jsonfile.readFileSync(path.join( __dirname, '/package.json'));
+    console.log(`buildbroker, version ${package.version}`);
+    process.exit(0);
+}
+    
 if (process.argv.length < 3){
     console.log('Missing argument - broker [client|slave|coordinator]');
     process.exit(1);
