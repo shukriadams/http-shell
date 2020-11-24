16725089437d9906513337adc81df80af8be31a7
Shukri Adams <shukri.adams@gmail.com>
2020-11-24 01:18:44 +0100

    updated build script

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,25 @@
-# build 
-pkg ./../src/. --targets node10-linux-x64 --output ./linux64/buildbroker &&
-pkg ./../src/. --targets node10-windows-x64 --output ./win64/buildbroker.exe &&
+set -e # fail on errors
+
+  
+TARGET=linux
+while [ -n "$1" ]; do 
+    case "$1" in
+    --target)
+        TARGET="$2" shift;;
+    esac 
+    shift
+done
+
+if [ $TARGET == "linux" ]; then
+    pkg ./../src/. --targets node10-linux-x64 --output ./linux64/buildbroker 
+fi
+
+if [ $TARGET == "windows" ]; then
+    pkg ./../src/. --targets node10-windows-x64 --output ./win64/buildbroker.exe 
+fi
+
 # run app and ensure exit code was 0
-./build/buildbroker --version &&
+./build/buildbroker --version 
 if [ $? -eq 0 ]
 then
   echo "App built"
