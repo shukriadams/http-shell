8a6d61d34c1d9a0bbd368235c50f34705eda0f46
egomeh <jensgholm15@gmail.com>
2021-12-15 13:43:14 +0100

    Fix sending kill command on client exit

--- src/lib/client.js
+++ src/lib/client.js
@@ -53,12 +53,11 @@ const process = require('process'),
             if (pid){
                 console.log(`${settings.protocol}://${workerHost}:${settings.port}/v1/pkill/${pid}`)
                 console.log(`Send remote pkill for "${pid}"`)
-                (function(){
-                    let exec = require('child_process')
-                    exec.execSync(`curl ${settings.protocol}://${workerHost}:${settings.port}/v1/pkill/${pid}`, function (error, stderr) {
-                        // we don't care about errors
-                    })
-                })()
+                
+                let exec = require('child_process')
+                exec.execSync(`curl ${settings.protocol}://${workerHost}:${settings.port}/v1/pkill/${pid}`, function (error, stderr) {
+                    // we don't care about errors
+                })
             }
 
             process.exit(1)
